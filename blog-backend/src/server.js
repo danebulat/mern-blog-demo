64fcdf8a04ee330849a14a48b93994af9cf42747
Dane Bulat <dane.bulat@gmail.com>
2023-05-18 17:57:45 +0100

    static build path set

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -69,7 +69,7 @@ app.use(async (req, res, next) => {
 // -------------------------------------------------- 
 
 app.get(/^(?!\/api).+/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 // -------------------------------------------------- 
